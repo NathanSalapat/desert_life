61f8e1ff4b51cf5c7b959df8c3d86e91c5e0e9b7
NathanSalapat <rendertracer@gmail.com>
2016-07-17 17:35:47 -0500

    Prickly Pears bloom in spring if mymonths is enabled.

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 desert_life = {}
 
+if minetest.get_modpath('mymonths') then
+   desert_life_bloom = true
+end
+
 dofile(minetest.get_modpath('desert_life')..'/functions.lua')
 dofile(minetest.get_modpath('desert_life')..'/prickly_pear.lua')
 dofile(minetest.get_modpath('desert_life')..'/barrel_cacti.lua')
--- prickly_pear.lua
+++ prickly_pear.lua
@@ -53,7 +53,7 @@ for i in ipairs (prickly_pear_table) do
      drawtype = 'mesh',
      mesh = 'dl_pp_'..num..'.obj',
      tiles = {name='dl_prickly_pear.png'},
-     groups = {not_in_creative_inventory=1},
+     groups = {not_in_creative_inventory=1, dl_pp=1},
      paramtype = 'light',
      paramtype2 = 'facedir',
      selection_box = col,
@@ -73,6 +73,35 @@ for i in ipairs (prickly_pear_table) do
         end
      end
      })
+
+     if desert_life_bloom == true then
+        minetest.register_node('desert_life:prickly_pear_'..num..'_bloom', {
+           description = 'Blooming Prickly Pear',
+           drawtype = 'mesh',
+           mesh = 'dl_pp_'..num..'.obj',
+           tiles = {name='dl_prickly_pear_bloom.png'},
+           groups = {not_in_creative_inventory=1, dl_pp=1},
+           paramtype = 'light',
+           paramtype2 = 'facedir',
+           selection_box = col,
+           collision_box = col,
+           on_punch = function(pos, node, player, pointed_thing)
+             minetest.set_node(pos, {name = AD..'_bloom', param2 = node.param2})
+             player:get_inventory():add_item('main', 'desert_life:prickly_pear') --If inventory is full it should be dropped.
+   --        local stack = ItemStack('desert_life:prickly_pear')
+   --        local leftover = minetest.add_item('main', stack)
+   --        if leftover:get_count() > 0 then
+   --           minetest.add_item (pos, 'desert_life:prickly_pear '..leftover:get_count())
+   --        end
+            local damage_chance = math.random(1,15)
+            if damage_chance == 1 then
+               local hp = player:get_hp()
+               player:set_hp(hp - 1)
+            end
+         end
+         })
+     end
+
 end
 
 
@@ -145,6 +174,65 @@ minetest.register_abm{
       end
       if node.name == 'desert_life:prickly_pear_7' then
          desert_life.spread('desert_life:prickly_pear_1', pos, 'default:desert_sand', 'air')
-   end
-end,
+      end
+   end,
 }
+
+if desert_life_bloom == true then
+   minetest.register_abm{
+      nodenames = {"group:dl_pp"},
+      interval = 30,
+      chance = 20,
+      action = function(pos)
+         if mymonths.month_counter == 4
+         or mymonths.month_counter == 5
+         or mymonths.month_counter == 6 then
+            local node = minetest.get_node(pos)
+            if node.name == 'desert_life:prickly_pear_1' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_1_bloom", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_2' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_2_bloom", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_3' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_3_bloom", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_4' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_4_bloom", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_5' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_5_bloom", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_6' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_6_bloom", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_7' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_7_bloom", param2 = node.param2})
+            end
+         else
+            local node = minetest.get_node(pos)
+            if node.name == 'desert_life:prickly_pear_1_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_1", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_2_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_2", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_3_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_3", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_4_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_4", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_5_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_5", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_6_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_6", param2 = node.param2})
+            end
+            if node.name == 'desert_life:prickly_pear_7_bloom' then
+      		minetest.set_node(pos, {name = "desert_life:prickly_pear_7", param2 = node.param2})
+            end
+         end
+      end,
+}
+end
