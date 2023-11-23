7dc671ca9df8b8c2b3a8603c5fb797c146b3b7cd
Nathan Salapat <nathan@nathansalapat.com>
2023-11-22 18:12:13 -0600

    Code cleanup.

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 desert_life = {}
+if minetest.get_modpath('mymonths') then
+   desert_life.bloom = true
+end
 dofile(minetest.get_modpath('desert_life')..'/functions.lua')
 dofile(minetest.get_modpath('desert_life')..'/prickly_pear.lua')
 dofile(minetest.get_modpath('desert_life')..'/barrel_cacti.lua')
